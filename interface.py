d811cc3d8a379d7b4e447f792a59ca770997f23a
1176850634@qq.com <1176850634@qq.com>
2022-11-06 13:51:50 +0800

    1.0 Version

--- interface.py
+++ interface.py
@@ -4,20 +4,23 @@ import run_test
 
 def interface():
     input_dic = 'C:\\Users\\GJC\\Desktop\\input'
-    output_dic = 'NULL'
-    choose = ''
+    output_dic = 'C:\\Users\\GJC\\Desktop\\output'
+    testcases = 10
     while True:
         print('欢迎：')
         print('    1. 更改待测试文件文件夹，当前:' + input_dic)
         print('    2. 更改输出文件夹，当前:' + output_dic)
-        print('    3. 运行测试')
+        print('    3. 更改随机生成测试样例数，当前:' + str(testcases))
+        print('    4. 运行测试')
         choose = input()
         if choose == '1':
             input_dic = input_input_dic()
         elif choose == '2':
             output_dic = input_output_dic()
         elif choose == '3':
-            start_test(input_dic, output_dic)
+            testcases = input_testcase()
+        elif choose == '4':
+            start_test(input_dic, output_dic, testcases)
         else:
             print('指令无效！请重新输入。')
 
@@ -48,7 +51,25 @@ def input_output_dic():
     return ret
 
 
-def start_test(source_dic, output_path):
-    run_test.run(source_dic, output_path)
+def input_testcase():
+    os.system('cls')
+    ret = ''
+    while True:
+        print('请输入随机生成的测试样例数:')
+        ret = input()
+        try:
+            ret = int(ret)
+        except:
+            print('请输入一个整数！')
+            continue
+        if ret <= 0:
+            print('请输入一个正整数！')
+            continue
+        break
+    return ret
+
+
+def start_test(source_dic, output_path, testcases):
+    run_test.run(source_dic, output_path, testcases)
 
 
